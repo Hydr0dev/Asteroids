6b271a35dd42c9c1e9742c289a43ec38e02d3daa
Hydr0dev <e.lemay514@gmail.com>
2026-01-03 18:43:52 -0500

    Added player and rotation

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED)
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,8 +15,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        
+
 if __name__ == "__main__":
     main()
