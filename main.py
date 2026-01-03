0117f83f1e0e4e9736e9f68b4c34e440378d6aa7
Hydr0dev <e.lemay514@gmail.com>
2026-01-03 15:30:00 -0500

    Decoupling of game speed and draw speed

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from logger import log_state
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -13,6 +15,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60)/1000
+        
 if __name__ == "__main__":
     main()
